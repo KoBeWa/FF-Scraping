19073cfb7d46b0f55f2dc048670ae07b74f197dc
KoBeWa <benneyftw@gmail.com>
2025-09-30 09:39:22 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 leagueID = "3082897"
-leagueStartYear = 2013
-leagueEndYear = 2024
+leagueStartYear = 2015
+leagueEndYear = 2021
 
 standings_directory = './output/'+ leagueID + '-history-standings/'
 gamecenter_directory = './output/'+ leagueID + '-history-teamgamecenter/'
